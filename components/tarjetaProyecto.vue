44c2c4181e7499ed03bd1ba0d579adfd146014cf
IanEvers <59263788+IanEvers@users.noreply.github.com>
2022-03-09 20:18:53 -0300

    ahora el link anda en la imagen del proyecto

--- components/tarjetaProyecto.vue
+++ components/tarjetaProyecto.vue
@@ -6,10 +6,11 @@
     <hr class="w-full border-left-2 border-black">
     
     <div class="relative z-30">
-
-      <img class="h-48 w-full object-fill z-30" :src="imagen">
-      <a :href="url" target="_blank" class="botonUrl z-30">
-        <button class="z-30">
+      <a :href="url" target="_blank" class=" ">
+        <img class="h-48 w-full object-fill z-30" :src="imagen">
+      </a>
+      <a :href="url" target="_blank" class=" z-30">
+        <button class="z-30 botonUrl">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="white" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
           </svg>
@@ -53,7 +54,6 @@ export default {
   right: 25px;
   background-color: #231a26;
   padding: 0.5rem;
-  padding-bottom: 0rem;
 }
 
 .descripcion {
