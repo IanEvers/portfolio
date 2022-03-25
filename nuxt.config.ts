14b563f4b753d4cad0b04ff652d63d1454c70d90
IanEvers <59263788+IanEvers@users.noreply.github.com>
2022-03-24 22:04:15 -0300

    arreglo por ahora

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,5 @@ import { defineNuxtConfig } from 'nuxt3'
 export default defineNuxtConfig({
   css: ['~/assets/scss/styles.scss'],
   buildModules: ['@nuxtjs/tailwindcss'],
-  ssr: true,
+  ssr: false,
 })
