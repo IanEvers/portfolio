bf90294bb1ceeab0713344342c113b64132425e7
IanEvers <59263788+IanEvers@users.noreply.github.com>
2022-03-28 17:02:53 -0300

    soy tonto

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,19 +21,6 @@ export default defineNuxtConfig({
       { property: "twitter:description", content: "Portfolio de Ian Evers." },
       { property: "twitter:image", content: "https://ian-broker.vercel.app/portfolio.png" },
     ],
-    script: [
-      // <script src="https://myawesome-lib.js"></script>
-      { src: 'https://awesome-lib.js' }
-    ],
-    link: [
-      // <link rel="stylesheet" href="https://myawesome-lib.css">
-      { rel: 'stylesheet', href: 'https://awesome-lib.css' }
-    ],
-    // please note that this is an area that is likely to change
-    style: [
-      // <style type="text/css">:root { color: red }</style>
-      { children: ':root { color: red }', type: 'text/css' }
-    ]
    }
 })
 
